bd333d0dc321cab2169df778910dc52c6ecc9485
callumbro <callumbro@hotmail.com>
2018-03-17 02:48:39 -0400

    Added page switching using bottom navigation bar.

--- app/src/main/java/com/pizzaordermobile/pizzaordermobile/MainActivity.java
+++ app/src/main/java/com/pizzaordermobile/pizzaordermobile/MainActivity.java
@@ -3,6 +3,8 @@ package com.pizzaordermobile.pizzaordermobile;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -10,20 +12,31 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    // create the fragments
+    private NotificationFragment notificationFragment = new NotificationFragment();
+    private HomeFragment homeFragment = new HomeFragment();
+    private FragmentManager fragmentManager = getSupportFragmentManager();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    fragmentTransaction.replace(R.id.fragment_container, homeFragment).commit();
+
                     mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
+                    fragmentTransaction.replace(R.id.fragment_container, notificationFragment).commit();
+
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
@@ -39,6 +52,9 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, homeFragment).commit();
     }
 
 }
